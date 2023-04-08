8e948d56d0ef958d45922b8fdfb25474413173e3
Davis <slag713@gmail.com>
2023-04-08 10:11:47 -0400

    secrets

--- beachnetworking/settings.py
+++ beachnetworking/settings.py
@@ -22,9 +22,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-inn&x4gm347kh19&c3vc6!8=61x^!9g8i(#z!+#h=pkgx+#%ig'
 
+# SECURITY WARNING: keep the secret key used in production secret!
+
+SECKEY = os.getenv("SEC_KEY")
+
+SECRET_KEY = SECKEY
 # SECURITY WARNING: don't run with debug turned on in production!
 
 DEBUG = os.environ.get('DEBUG', 'false').lower() == 'true'
@@ -172,10 +175,11 @@ CRISPY_TEMPLATE_PACK = "bootstrap5"
 LOGIN_REDIRECT_URL = '/'
 LOGIN_URL = 'login'
 
+EMAILPWORDS = os.getenv("EMAIL_PWORD")
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'no-reply@beachnetworking.com'
-EMAIL_HOST_PASSWORD = 'HF6-n~BxjDC28Jx'
+EMAIL_HOST_PASSWORD = EMAILPWORDS
