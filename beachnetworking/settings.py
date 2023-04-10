1fbac4c5d015a9fa979285fbcd117ca144dd0674
Davis <slag713@gmail.com>
2023-04-10 09:06:31 -0400

    changed debug setting

--- beachnetworking/settings.py
+++ beachnetworking/settings.py
@@ -30,7 +30,7 @@ SECKEY = os.getenv("SEC_KEY")
 SECRET_KEY = SECKEY
 # SECURITY WARNING: don't run with debug turned on in production!
 
-DEBUG = os.environ.get('DEBUG', 'false').lower() == 'true'
+DEBUG = os.environ.get('DEBUG', 'False').lower() == 'True'
 
 
 ALLOWED_HOSTS = [
